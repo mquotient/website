d63216e339bc0ff2b2d68a58782d4c51a4a2907b
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2014-04-08 10:38:56 +0530

    Variable names changed, removed unused variables and done linting for proper indentation.

--- js/application.js
+++ js/application.js
@@ -5,21 +5,21 @@ function resizePages() {
 	sectionHeight  =  h < 600 ? 600 : h;
 	
 	// Each sections height
-	$('section#cover').css('height',sectionHeight);
-	$('section').css('min-height',sectionHeight);
+	$('section#cover').css('height', sectionHeight);
+	$('section').css('min-height', sectionHeight);
 	$('.wrapper:not(:last-child)').each(function () {
-		$(this).css('min-height',$(this).closest('section').height() - 100);
+		$(this).css('min-height', $(this).closest('section').height() - 100);
 	});
 
 	$('.wrapper:last-child').each(function () {
-		$(this).css('min-height',$(this).closest('section').height());
+		$(this).css('min-height', $(this).closest('section').height());
 	});
 
 	// Home Page height adjust
-	var bodyFontSize = $('body').css('font-size').slice(0,2),
-		mquotientFontSize = bodyFontSize * 8;
-	$('#covercontainer h1').css('padding-top',sectionHeight/2 - (mquotientFontSize * 1.3));
-	$('#covercontainer .navcontainer').css('padding-top',sectionHeight / 2 + 10);
+	var bodyFontSize = $('body').css('font-size').slice(0, 2);
+	var mquotientFontSize = bodyFontSize * 8;
+	$('#covercontainer h1').css('padding-top', sectionHeight / 2 - (mquotientFontSize * 1.3));
+	$('#covercontainer .navcontainer').css('padding-top', sectionHeight / 2 + 10);
 
 
 	// Make map full of height
@@ -27,8 +27,8 @@ function resizePages() {
 	$('#map').height($('#map').height() + $contactUsContainer.parent().height() - $contactUsContainer.height());
 }
 
-$(window).load(function() {
-	$('section:not(:first,:last, #team, #about)').each(function(){
+$(window).load(function () {
+	$('section:not(:first,:last, #team, #about)').each(function () {
 		var remainingSpace = $('#' + this.id + ' .wrapper').height() - $('#' + this.id + 'container').height(),
 			marginTop = remainingSpace / 2;
 
@@ -37,8 +37,7 @@ $(window).load(function() {
 
 });
 
-$(document).ready(function() {
-
+$(document).ready(function () {
 	var scrollElement = 'html, body';
 	$('html, body').each(function () {
 		var initScrollTop = $(this).attr('scrollTop');
@@ -47,22 +46,21 @@ $(document).ready(function() {
 			scrollElement = this.nodeName.toLowerCase();
 			$(this).attr('scrollTop', initScrollTop);
 			return false;
-		}    
+		}
 	});
-	$(".down a, .goTo").click(function(event) {
-		if($(this).hasClass('nav-item')) {
+	$(".down a, .goTo").click(function (event) {
+		if ($(this).hasClass('nav-item')) {
 			hideMenu();
 		}
 
 		event.preventDefault();
 		
-		var $this = $(this),
-		target = this.hash,
-		$target = $(target);
+		var target = this.hash,
+			$target = $(target);
 		
 		$(scrollElement).stop().animate({
 			'scrollTop': $target.offset().top
-		}, 300, 'swing', function() {
+		}, 300, 'swing', function () {
 			window.location.hash = target;
 		});
 	});
@@ -83,92 +81,89 @@ $(document).ready(function() {
 		$("#floating-menu .menu-btn").addClass('shown');
 	}
 
-	menuBtn = document.getElementById("menu-btn");
-	menuBtn.onclick = function() {
-		if($navItem.first().hasClass('shown')) {
+	var menuBtn = document.getElementById("menu-btn");
+	
+	menuBtn.onclick = function () {
+		if ($navItem.first().hasClass('shown')) {
 			hideMenu();
 		}
 		else {
-			showMenu();			
+			showMenu();
 		}
-	}
+	};
 
 	//resize
-	$(window).resize(function(e) {
+	$(window).resize(function () {
 		resizePages();
 	});
 
 	resizePages();
 
 	//scroll
-	$(window).scroll(_.throttle(function(e){
+	$(window).scroll(_.throttle(function () {
         var top = $(document).scrollTop();
-		var wHeight = Math.max(640,$(window).height());
 		// Shift navigation element to top
-		if(top >= (sectionHeight / 2 + $('.navcontainer').height())) {
-			
-
-			if($(window).width() <= 768) {
+		if (top >= (sectionHeight / 2 + $('.navcontainer').height())) {
+			if ($(window).width() <= 768) {
 				$("#floating-menu").fadeIn();
-			}
-			else {
-				$('#footer').css({
+			} else {
+				$('#menu').css({
 					opacity: 1,
-					top: 0
+					top: -1
 				});
 			}
 		}
 		else {
-			if($(window).width() <= 768) {
+			if ($(window).width() <= 768) {
 				$("#floating-menu").fadeOut();
 			}
 			else {
-				$('#footer').css({
+				$('#menu').css({
 					opacity: 0,
 					top: -30
 				});
-			}			
+			}
 		}
 
 		$('section:not(:last)').each(function () {
-			if((pageYOffset + 200) >= this.offsetTop && (pageYOffset + 200) < $(this).next()[0].offsetTop) {
+			if ((pageYOffset + 200) >= this.offsetTop && (pageYOffset + 200) < $(this).next()[0].offsetTop) {
 				var self = this;
-				$('#footer li a:not(:first)').each(function(){
-					if($(this).data('id') == self.id) {
-						$('#footer .active').removeClass('active').parent().removeClass('active');	
-						$(this).addClass('active').parent().addClass('active');	
+				$('#menu li a:not(:first)').each(function () {
+					if ($(this).data('id') == self.id) {
+						$('#menu .active').removeClass('active').parent().removeClass('active');
+						$(this).addClass('active').parent().addClass('active');
 					}
 				});
 			}
 		});
 
-		if((pageYOffset + 200) > $('section:last')[0].offsetTop) {
-			$('#footer .active').removeClass('active').parent().removeClass('active');	
-			$('#footer a[data-id="contactus"]').addClass('active').parent().addClass('active');	
+		if ((pageYOffset + 200) > $('section:last')[0].offsetTop) {
+			$('#menu .active').removeClass('active').parent().removeClass('active');
+			$('#menu a[data-id="contactus"]').addClass('active').parent().addClass('active');
 		}
     }, 100));
 
-	if($(window).width() <= 768) {
+	if ($(window).width() <= 768) {
 		$("#floating-menu").hide();
 	}
 });
 
 // Load Google Maps
-function initialize() {
-	var ourLocation = new google.maps.LatLng(18.53585,73.885988);
+function initializeMaps() {
+	var ourLocation = new google.maps.LatLng(18.53585, 73.885988);
 	var mapOptions = {
 		zoom: 13,
 		center: ourLocation,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
+	};
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	
 	var marker = new google.maps.Marker({
-		map:map,
+		map: map,
 		position: ourLocation
 	});
 
-	var contentString = '<div>'+
+	var contentString = '<div>' +
 		'<address>' +
 		'	<strong><span class="m">m</span><span class="q">quotient</span></strong>' +
 		'	<br>' +
@@ -195,22 +190,22 @@ function initialize() {
 		content: contentString
 	});
 
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.open(map,marker);
+	google.maps.event.addListener(marker, 'click', function () {
+		infowindow.open(map, marker);
 	});
 
-	google.maps.event.addListenerOnce(map, 'idle', function(){
+	google.maps.event.addListenerOnce(map, 'idle', function () {
 		$('.gmnoprint:last').remove();
 		$('.gmnoprint:first').remove();
 		$('.gmnoprint:first').remove();
 	});
 }
 
-function loadScript() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAFWFw3Rdf7fBuvwo4DTUV2DcTD5W7tBWc&sensor=true&callback=initialize";
-  document.body.appendChild(script);
+function loadMaps() {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAFWFw3Rdf7fBuvwo4DTUV2DcTD5W7tBWc&sensor=true&callback=initializeMaps";
+	document.body.appendChild(script);
 }
 
-window.onload = loadScript;
+window.onload = loadMaps;
