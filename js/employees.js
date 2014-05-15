7a0579de899e5fc849bfb752e94aa5ebaad405a9
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2014-05-15 23:58:03 +0530

    LiinkedIn, twitter profile links & Titles updated

--- js/employees.js
+++ js/employees.js
@@ -1,12 +1,12 @@
 var employees = [
-	{ "name": "Rishi", "sirname": "Behal", "position": "Founder & CEO", "shortname": "rishi", "linkedin": "http://www.linkedin.com/in/rishibehal", "twitter": "http://twitter.com/xyz"},
-	{ "name": "Sreejith", "sirname": "Narayanan", "position": "Founder & Chief Scientist", "shortname": "sreejith", "linkedin": "http://www.linkedin.com/in/SreejithNarayanan", "twitter": "http://twitter.com/xyz", "github": "http://github.com/sreejith-mq"},
-	{ "name": "Satyajit", "sirname": "Ranjeev", "position": "Team Leader", "shortname": "satyajit", "linkedin": "http://www.linkedin.com/in/satyajitranjeev", "twitter": "http://twitter.com/xyz", "github": "http://github.com/satyajit-mq"},
-	{ "name": "Yati", "sirname": "Sagade", "position": "Software Engineer", "shortname": "yati", "linkedin": "http://www.linkedin.com/in/yatisagade", "twitter": "https://twitter.com/yati_itay", "github": "http://github.com/yati-mq"},
-	{ "name": "Haridas", "sirname": "", "position": "Software Engineer", "shortname": "haridas", "linkedin": "http://www.linkedin.com/in/haridasNarayanaSwamy", "twitter": "http://twitter.com/haridasNarayanaSwamy", "github": "http://github.com/haridas-mq"},
-	{ "name": "Sandesh", "sirname": "D", "position": "Front End Web Developer", "shortname": "sandesh", "linkedin": "http://www.linkedin.com/in/sandeshdamkondwar", "twitter": "http://twitter.com/sandydamy", "github": "http://github.com/sandeshdamkondwar"},
-	{ "name": "Vijisha", "sirname": "Vijaya", "position": "HR Manager", "shortname": "vijisha", "linkedin": "http://www.linkedin.com/in/vijishavijaya", "twitter": "http://twitter.com/vijishavijaya"},
-	{ "name": "Mayur", "sirname": "Swamy", "position": "Software Engineer", "shortname": "mayur", "linkedin": "http://www.linkedin.com/in/mayurswamy", "twitter": "http://twitter.com/mayurswamy"},
-	{ "name": "Kiran", "sirname": "Kiran", "position": "Software Engineer", "shortname": "kiran", "linkedin": "https://www.linkedin.com/pub/kiran-p/76/580/132", "twitter": "https://twitter.com/kirankaladharan"},
-	{ "name": "Sreejith", "sirname": "Ramakrishnan", "position": "Associate Software Engineer", "shortname": "jr-sreejith", "linkedin": "http://www.linkedin.com/pub/sreejith-ramakrishnan/30/16b/a24", "twitter": "http://twitter.com/locomunkey", "github": "http://github.com/rsreejith-mq"}
+	{ "name": "Rishi", "surname": "Behal", "position": "Founder & CEO", "shortname": "rishi", "linkedin": "http://www.linkedin.com/in/rishibehal"},
+	{ "name": "Sreejith", "surname": "Narayanan", "position": "Founder & Chief Scientist", "shortname": "sreejith", "linkedin": "http://in.linkedin.com/in/sreejithnarayanan", "twitter": "http://twitter.com/sreejithhere", "github": "http://github.com/sreejith-mq"},
+	{ "name": "Satyajit", "surname": "Ranjeev", "position": "Team Leader", "shortname": "satyajit", "linkedin": "http://www.linkedin.com/in/satyajitranjeev", "twitter": "http://twitter.com/satyajitranjeev", "github": "http://github.com/satyajit-mq"},
+	{ "name": "Yati", "surname": "Sagade", "position": "Product Manager", "shortname": "yati", "linkedin": "http://in.linkedin.com/pub/yati-sagade/3b/b66/861", "twitter": "https://twitter.com/yati_itay", "github": "http://github.com/yati-mq"},
+	{ "name": "Haridas", "surname": "", "position": "Software Engineer", "shortname": "haridas", "linkedin": "http://in.linkedin.com/in/haridasn", "twitter": "http://twitter.com/haridas_n", "github": "http://github.com/haridas-mq"},
+	{ "name": "Sandesh", "surname": "D", "position": "Front End Web Developer", "shortname": "sandesh", "linkedin": "http://in.linkedin.com/in/sandeshdamkondwar", "twitter": "http://twitter.com/sandydamy", "github": "http://github.com/sandeshdamkondwar"},
+	{ "name": "Vijisha", "surname": "Vijaya", "position": "HR Manager", "shortname": "vijisha", "linkedin": "http://in.linkedin.com/pub/vijisha-v/1b/94a/b0a"},
+	{ "name": "Mayur", "surname": "Swamy", "position": "Software Engineer", "shortname": "mayur", "linkedin": "http://in.linkedin.com/pub/mayur-swami/18/58b/ab8"},
+	{ "name": "Kiran", "surname": "Kiran", "position": "Associate Software Engineer", "shortname": "kiran", "linkedin": "https://www.linkedin.com/pub/kiran-p/76/580/132", "twitter": "https://twitter.com/kirankaladharan"},
+	{ "name": "Sreejith", "surname": "Ramakrishnan", "position": "Associate Software Engineer", "shortname": "jr-sreejith", "linkedin": "http://www.linkedin.com/pub/sreejith-ramakrishnan/30/16b/a24", "twitter": "http://twitter.com/locomunkey", "github": "http://github.com/rsreejith-mq"}
 ];
