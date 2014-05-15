ed4126371f41dc25a63d05d0de394debda008a01
Sreejith Narayanan <sreejith@mquotient.net>
2014-05-15 13:07:09 +0530

    Updated my position

--- js/employees.js
+++ js/employees.js
@@ -1,6 +1,6 @@
 var employees = [
 	{ "name": "Rishi", "sirname": "Behal", "position": "Founder & CEO", "shortname": "rishi", "linkedin": "http://www.linkedin.com/in/rishibehal", "twitter": "http://twitter.com/xyz"},
-	{ "name": "Sreejith", "sirname": "Narayanan", "position": "Chief Scientist", "shortname": "sreejith", "linkedin": "http://www.linkedin.com/in/SreejithNarayanan", "twitter": "http://twitter.com/xyz", "github": "http://github.com/sreejith-mq"},
+	{ "name": "Sreejith", "sirname": "Narayanan", "position": "Founder & Chief Scientist", "shortname": "sreejith", "linkedin": "http://www.linkedin.com/in/SreejithNarayanan", "twitter": "http://twitter.com/xyz", "github": "http://github.com/sreejith-mq"},
 	{ "name": "Satyajit", "sirname": "Ranjeev", "position": "Team Leader", "shortname": "satyajit", "linkedin": "http://www.linkedin.com/in/satyajitranjeev", "twitter": "http://twitter.com/xyz", "github": "http://github.com/satyajit-mq"},
 	{ "name": "Yati", "sirname": "Sagade", "position": "Software Engineer", "shortname": "yati", "linkedin": "http://www.linkedin.com/in/yatisagade", "twitter": "https://twitter.com/yati_itay", "github": "http://github.com/yati-mq"},
 	{ "name": "Haridas", "sirname": "", "position": "Software Engineer", "shortname": "haridas", "linkedin": "http://www.linkedin.com/in/haridasNarayanaSwamy", "twitter": "http://twitter.com/haridasNarayanaSwamy", "github": "http://github.com/haridas-mq"},
