4e883206293fea6527310f5019ca334bc17fdb0d
Sandesh Damkondwar <sandeshdamkondwar@gmail.com>
2014-05-20 12:30:07 +0530

    Haridas's title changed to project manager.

--- js/employees.js
+++ js/employees.js
@@ -1,9 +1,9 @@
 var employees = [
 	{ "name": "Rishi", "surname": "Behal", "position": "Founder & Director", "shortname": "rishi", "linkedin": "http://www.linkedin.com/in/rishibehal"},
 	{ "name": "Sreejith", "surname": "Narayanan", "position": "Founder & Chief Scientist", "shortname": "sreejith", "linkedin": "http://in.linkedin.com/in/sreejithnarayanan", "twitter": "http://twitter.com/sreejithhere", "github": "http://github.com/sreejith-mq"},
-	{ "name": "Satyajit", "surname": "Ranjeev", "position": "Team Leader", "shortname": "satyajit", "linkedin": "http://www.linkedin.com/in/satyajitranjeev", "twitter": "http://twitter.com/satyajitranjeev", "github": "http://github.com/satyajit-mq"},
+	{ "name": "Satyajit", "surname": "Ranjeev", "position": "Team Leader", "shortname": "satyajit", "linkedin": "http://www.linkedin.com/in/satyajitranjeev", "twitter": "http://twitter.com/satyajitranjeev", "github": "http://github.com/satyajitranjeev"},
 	{ "name": "Yati", "surname": "Sagade", "position": "Product Manager", "shortname": "yati", "linkedin": "http://in.linkedin.com/pub/yati-sagade/3b/b66/861", "twitter": "https://twitter.com/yati_itay", "github": "http://github.com/yati-mq"},
-	{ "name": "Haridas", "surname": "", "position": "Software Engineer", "shortname": "haridas", "linkedin": "http://in.linkedin.com/in/haridasn", "twitter": "http://twitter.com/haridas_n", "github": "http://github.com/haridas-mq"},
+	{ "name": "Haridas", "surname": "", "position": "Project Manager", "shortname": "haridas", "linkedin": "http://in.linkedin.com/in/haridasn", "twitter": "http://twitter.com/haridas_n", "github": "http://github.com/haridas-mq"},
 	{ "name": "Sandesh", "surname": "D", "position": "Front End Web Developer", "shortname": "sandesh", "linkedin": "http://in.linkedin.com/in/sandeshdamkondwar", "twitter": "http://twitter.com/sandydamy", "github": "http://github.com/sandeshdamkondwar"},
 	{ "name": "Vijisha", "surname": "Vijaya", "position": "HR Manager", "shortname": "vijisha", "linkedin": "http://in.linkedin.com/pub/vijisha-v/1b/94a/b0a"},
 	{ "name": "Mayur", "surname": "Swamy", "position": "Software Engineer", "shortname": "mayur", "linkedin": "http://in.linkedin.com/pub/mayur-swami/18/58b/ab8"},
